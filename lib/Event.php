horde/kronolith

commit 09d90141292f9ec516a7a2007bf828ce2bbdf60d

Bug: 14857
Prevent XSS in event's URL field.

lib/Event.php
@@ -2402,8 +2402,8 @@ abstract class Kronolith_Event
             }
             if (!$this->isPrivate()) {
                 $json->d = $this->description;
-                $json->u = $this->url;
-                $json->uhl = $GLOBALS['injector']->getInstance('Horde_Core_Factory_TextFilter')->filter($this->url, 'linkurls');
+                $json->u =  htmlentities($this->url);
+                $json->uhl = htmlentities($GLOBALS['injector']->getInstance('Horde_Core_Factory_TextFilter')->filter($this->url, 'linkurls'));
                 $json->tg = array_values($this->tags);
                 $json->gl = $this->geoLocation;
                 if ($this->attendees) {

commit 9ae8239350eae077c3f57a3ae5c408965334fb1a

Bug: 14941  Fix regression in display of clickable link introduced by:
https://github.com/horde/kronolith/commit/09d90141292f9ec516a7a2007bf828ce2bbdf60d

lib/Event.php
@@ -2919,7 +2919,10 @@ abstract class Kronolith_Event
             if (!$this->isPrivate()) {
                 $json->d = $this->description;
                 $json->u =  htmlentities($this->url);
-                $json->uhl = htmlentities($GLOBALS['injector']->getInstance('Horde_Core_Factory_TextFilter')->filter($this->url, 'linkurls'));
+                $json->uhl = $GLOBALS['injector']->getInstance('Horde_Core_Factory_TextFilter')->filter(
+                    $GLOBALS['injector']->getInstance('Horde_Core_Factory_TextFilter')->filter($this->url, 'linkurls'),
+                    'Xss'
+                );
                 $json->tg = array_values($this->tags);
                 $json->gl = $this->geoLocation;
                 if (count($this->attendees)) {