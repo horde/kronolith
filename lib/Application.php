55e9cac7b02a2caa3245a62c58f933f8daf4efd0
Ralf Lang <lang@b1-systems.de>
2021-02-24 14:57:50 +0000

    Released kronolith-5.0.0alpha1

--- lib/Application.php
+++ lib/Application.php
@@ -48,7 +48,7 @@ class Kronolith_Application extends Horde_Registry_Application
 
     /**
      */
-    public $version = '5.0.0alpha1';
+    public $version = 'H5 (5.0.0alpha1)';
 
     /**
      * Global variables defined:
