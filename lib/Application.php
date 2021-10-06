edd7004393e6e30e04727efba1fea290a36ade42
Ralf Lang <lang@b1-systems.de>
2021-10-06 15:05:18 +0000

    Development mode for kronolith-5.0.0alpha5

--- lib/Application.php
+++ lib/Application.php
@@ -48,7 +48,7 @@ class Kronolith_Application extends Horde_Registry_Application
 
     /**
      */
-    public $version = 'H5 (5.0.0alpha4)';
+    public $version = 'H5 (5.0.0alpha5)';
 
     /**
      * Global variables defined:
