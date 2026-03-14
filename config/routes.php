102e19abf060a4cc1ab484f3cec7b8622f65172e
Ralf Lang <ralf.lang@ralf-lang.de>
2026-03-14 07:30:38 +0100

    fix: Commit metadata after components mishap.

---
+++ config/routes.php
@@ -0,0 +1,2 @@
+<?php
+// Empty default routes file. Put custom routes into var/config/kronolith/routes.local.php and run composer horde:reconfigure
--- lib/Application.php
+++ lib/Application.php
@@ -49,7 +49,7 @@ class Kronolith_Application extends Horde_Registry_Application
 
     /**
      */
-    public $version = '5.0.0-alpha9';
+    public $version = '5.0.0-alpha10';
 
     /**
      * Global variables defined:
