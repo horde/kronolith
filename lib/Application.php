c67bd8a3022c931d94a803b99bad60c94de8bce4
Ralf Lang <ralf.lang@ralf-lang.de>
2026-04-27 22:16:10 +0200

    chore(release): bump version to 5.0.0-beta3
    Release version 5.0.0-beta3
    
    fix: Wrong level of urlencode
    fix: Catch Sabre\HTTP\ClientHttpException in isCalDAV()
    test: Update to PHPUnit 11 and add a CalendarsManagerTest
    fix: Harden against half-installed Date_Holidays
    fix(js): classList.remove() does not return element for chaining
    refactor(intermediate): Remove about 25% PrototypeJs from non-leaf javascript
    docs: Provide default config file for kronolith
    refactor(js): embed, goto and views helper converted from prototypejs to vanilla js
    fix: Make recurrences mutation safe by explicit cloning
    chore: Temporary commit, dubious after beta1 release

--- lib/Application.php
+++ lib/Application.php
@@ -51,7 +51,7 @@ class Kronolith_Application extends Horde_Registry_Application
 
     /**
      */
-    public $version = '5.0.0-beta2';
+    public $version = '5.0.0-beta3';
 
     /**
      * Global variables defined:
