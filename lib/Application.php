8f24dfbbb9c4592d6454b271509d0e2f4a64487d
Ralf Lang <ralf.lang@ralf-lang.de>
2026-05-12 20:28:42 +0200

    chore(release): bump version to 5.0.0-RC1
    Release version 5.0.0-RC1
    
    style: Add CSS rules for event form icons

--- lib/Application.php
+++ lib/Application.php
@@ -51,7 +51,7 @@ class Kronolith_Application extends Horde_Registry_Application
 
     /**
      */
-    public $version = '5.0.0-beta4';
+    public $version = '5.0.0-RC1';
 
     /**
      * Global variables defined:
