857af8ce0df3d777907c6da226b73f2f680a0679
Ralf Lang <ralf.lang@ralf-lang.de>
2026-05-19 19:58:34 +0200

    chore(release): bump version to 5.0.0-RC2
    Release version 5.0.0-RC2
    
    fix: Don't crash dynamic UI when viewing a task without an organizer or assignee
    fix: resize color picker icon in css
    refactor(injector): Widen type hints from Horde_Injector to Horde_Injector|Injector
    Merge pull request #39 from horde/refactor/strftime-icu-migration
    fix(kronolith): Use D_FMT for sd/ed JSON fields to match Date.js parser
    refactor(strftime): Migrate to native usage of IntlDateFormatter and DateTime
    Refactor background properties for sidebar icons
    style: DatePicker icons need to be resized/shrunk

--- lib/Application.php
+++ lib/Application.php
@@ -51,7 +51,7 @@ class Kronolith_Application extends Horde_Registry_Application
 
     /**
      */
-    public $version = '5.0.0-RC1';
+    public $version = '5.0.0-RC2';
 
     /**
      * Global variables defined:
