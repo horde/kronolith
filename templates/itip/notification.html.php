d2b3390f55bca40e322947254719be22ed311162
Torben Dannhauer <torben@dannhauer.de>
2026-06-23 13:16:43 +0200

    feat(kronolith): simplify iTIP invitation mail and templates
    Build invitation MIME via MimeEnvelopeBuilder, remove duplicate
    application/ics attachment and embedded icon, refresh notification
    templates, show recurrence in mail bodies, and add Invitation subject
    translations for all supported locales.

--- templates/itip/notification.html.php
+++ templates/itip/notification.html.php
@@ -1,88 +1,70 @@
-<p><font size="4"><strong><?php echo $this->h($this->header) ?></strong></font></p>
-<table width="100%" border="0" cellspacing="0" cellpadding="0"><tr>
-  <td width="140" valign="top">
-    <img src="cid:<?php echo $this->imageId ?>" />
-  </td>
-  <td valign="top">
-    <table width="100%" border="0" cellpadding="5" cellspacing="0">
-      <thead><tr>
-        <th colspan="3" align="left"><font size="3"><strong><?php echo $this->h($this->event->getTitle()) ?></strong></font></th>
-      </tr></thead>
-      <tbody>
-        <?php $i = 1 ?>
+<div class="kronolith-itip-notification" style="font-family: Arial, Helvetica, sans-serif; max-width: 640px; color: #222;">
+  <h2 style="margin: 0 0 12px; font-size: 20px; font-weight: 600;"><?php echo $this->h($this->header) ?></h2>
 
-        <tr<?php if ($i++ % 2) {
-            echo ' bgcolor="#f1f1f1"';
-        } ?>>
-          <td nowrap="nowrap" align="right">
-            <font size="2"><strong><?php echo _("Start:") ?></strong></font>
-          </td>
-          <td width="5">&nbsp;</td>
-          <td width="100%"><font size="2"><strong><?php echo (new IntlDateFormatter($GLOBALS['language'], IntlDateFormatter::SHORT, IntlDateFormatter::MEDIUM))->format($this->event->start->timestamp()) ?></strong></font></td>
-        </tr>
-        <tr<?php if ($i++ % 2) {
-            echo ' bgcolor="#f1f1f1"';
-        } ?>>
-          <td nowrap="nowrap" align="right">
-            <font size="2"><strong><?php echo _("End:") ?></strong></font>
-          </td>
-          <td width="5">&nbsp;</td>
-          <td width="100%"><font size="2"><strong><?php echo (new IntlDateFormatter($GLOBALS['language'], IntlDateFormatter::SHORT, IntlDateFormatter::MEDIUM))->format($this->event->end->timestamp()) ?></strong></font></td>
-        </tr>
-        <?php if (strlen($this->event->location)): ?>
+  <table style="width: 100%; border-collapse: collapse; margin: 0 0 16px;">
+    <tr>
+      <td style="padding: 8px 12px 8px 0; vertical-align: top; width: 110px; color: #555; font-size: 14px;"><?php echo _("When") ?></td>
+      <td style="padding: 8px 0; font-size: 14px;">
+        <strong><?php echo (new IntlDateFormatter($GLOBALS['language'], IntlDateFormatter::SHORT, IntlDateFormatter::MEDIUM))->format($this->event->start->timestamp()) ?></strong>
+        –
+        <strong><?php echo (new IntlDateFormatter($GLOBALS['language'], IntlDateFormatter::SHORT, IntlDateFormatter::MEDIUM))->format($this->event->end->timestamp()) ?></strong>
+      </td>
+    </tr>
+    <?php if (strlen($this->recurrence)): ?>
+    <tr>
+      <td style="padding: 8px 12px 8px 0; vertical-align: top; color: #555; font-size: 14px;"><?php echo _("Recurrence") ?></td>
+      <td style="padding: 8px 0; font-size: 14px;"><?php echo nl2br($this->h($this->recurrence)) ?></td>
+    </tr>
+    <?php endif ?>
+    <?php if (strlen($this->event->location)): ?>
+    <tr>
+      <td style="padding: 8px 12px 8px 0; vertical-align: top; color: #555; font-size: 14px;"><?php echo _("Location") ?></td>
+      <td style="padding: 8px 0; font-size: 14px;"><strong><?php echo $this->h($this->event->location) ?></strong></td>
+    </tr>
+    <?php endif ?>
+    <?php if (!empty($this->organizer)): ?>
+    <tr>
+      <td style="padding: 8px 12px 8px 0; vertical-align: top; color: #555; font-size: 14px;"><?php echo _("Organizer") ?></td>
+      <td style="padding: 8px 0; font-size: 14px;"><strong><?php echo $this->h($this->organizer) ?></strong></td>
+    </tr>
+    <?php endif ?>
+    <?php if (count($this->event->attendees)): ?>
+    <tr>
+      <td style="padding: 8px 12px 8px 0; vertical-align: top; color: #555; font-size: 14px;"><?php echo _("Attendees") ?></td>
+      <td style="padding: 8px 0; font-size: 14px;">
+        <?php foreach ($this->event->attendees as $attendee): ?>
+        <div>
+          <?php if (is_null($attendee->addressObject->host)): ?>
+          <?php echo $this->h($attendee->displayName) ?>
+          <?php else: ?>
+          <a href="mailto:<?php echo $this->h($attendee->email) ?>"><?php echo $this->h($attendee->displayName) ?></a>
+          <?php endif ?>
+        </div>
+        <?php endforeach ?>
+      </td>
+    </tr>
+    <?php endif ?>
+    <?php if (strlen($this->event->description)): ?>
+    <tr>
+      <td style="padding: 8px 12px 8px 0; vertical-align: top; color: #555; font-size: 14px;"><?php echo _("Description") ?></td>
+      <td style="padding: 8px 0; font-size: 14px;">
+        <?php echo $GLOBALS['injector']->getInstance('Horde_Core_Factory_TextFilter')->filter($this->event->description, 'text2html', ['parselevel' => Horde_Text_Filter_Text2html::MICRO, 'callback' => null]) ?>
+      </td>
+    </tr>
+    <?php endif ?>
+  </table>
 
-        <tr<?php if ($i++ % 2) {
-            echo ' bgcolor="#f1f1f1"';
-        } ?>>
-          <td nowrap="nowrap" align="right">
-            <font size="2"><strong><?php echo _("Location:") ?></strong></font>
-          </td>
-          <td width="5">&nbsp;</td>
-          <td width="100%"><font size="2"><strong><?php echo $this->h($this->event->location) ?></strong></font></td>
-        </tr>
-        <?php endif ?>
-        <?php if (count($this->event->attendees)): ?>
-
-        <tr<?php if ($i++ % 2) {
-            echo ' bgcolor="#f1f1f1"';
-        } ?>>
-          <td nowrap="nowrap" align="right" valign="top">
-            <font size="2"><strong><?php echo _("Attendees:") ?></strong></font>
-          </td>
-          <td width="5">&nbsp;</td>
-          <td width="100%"><font size="2"><strong>
-            <?php foreach ($this->event->attendees as $attendee): ?>
-            <?php if (is_null($attendee->addressObject->host)): ?>
-            <?php echo $this->h($attendee->displayName) ?><br />
-            <?php else: ?>
-            <a href="mailto:<?php echo $this->h($attendee->email) ?>"><?php echo $this->h($attendee->displayName) ?></a><br />
-            <?php endif ?>
-            <?php endforeach ?>
-
-          </strong></font></td>
-        </tr>
-        <?php endif ?>
-
-        <?php if (strlen($this->event->description)): ?>
-
-        <tr<?php if ($i++ % 2) {
-            echo ' bgcolor="#f1f1f1"';
-        } ?>>
-          <td nowrap="nowrap" align="right" valign="top">
-            <font size="2"><strong><?php echo _("Description:") ?></strong></font>
-          </td>
-          <td width="5">&nbsp;</td>
-          <td width="100%"><font size="2"><strong><?php echo $GLOBALS['injector']->getInstance('Horde_Core_Factory_TextFilter')->filter($this->event->description, 'text2html', ['parselevel' => Horde_Text_Filter_Text2html::MICRO, 'callback' => null]) ?></strong></font></td>
-        </tr>
-        <?php endif ?>
-
-      </tbody>
-    </table>
-  </td>
-</tr></table>
-
-<p><font size="2"><?php echo _("Attached is an iCalendar file with more information about the event. If your mail client supports iTip requests you can use this file to easily update your local copy of the event.") ?></font></p>
-
-<?php if ($this->linkAccept): ?>
-<p><font size="2"><?php printf(_("If your email client doesn't support iTip requests you can use the following links to: %saccept%s, %saccept tentatively%s or %sdecline%s the event."), '<strong><a href="' . htmlspecialchars($this->linkAccept) . '">', '</a></strong>', '<strong><a href="' . htmlspecialchars($this->linkTentative) . '">', '</a></strong>', '<strong><a href="' . htmlspecialchars($this->linkDecline) . '">', '</a></strong>') ?></font></p>
-<?php endif ?>
+  <?php if ($this->linkAccept): ?>
+  <p style="font-size: 14px; margin: 16px 0 0;">
+    <?php printf(
+        _("If your email client does not show invitation actions, you can %saccept%s, respond %stentatively%s, or %sdecline%s online."),
+        '<a href="' . htmlspecialchars($this->linkAccept) . '"><strong>',
+        '</strong></a>',
+        '<a href="' . htmlspecialchars($this->linkTentative) . '"><strong>',
+        '</strong></a>',
+        '<a href="' . htmlspecialchars($this->linkDecline) . '"><strong>',
+        '</strong></a>'
+    ) ?>
+  </p>
+  <?php endif ?>
+</div>
--- templates/itip/notification.plain.php
+++ templates/itip/notification.plain.php
@@ -1,43 +1,44 @@
-<?php echo $this->subject ?> (<?php printf(_("on %s at %s"), $this->event->start->format('short', new Horde\Date\Formatter\IcuFormatter(), $GLOBALS['language']), (new IntlDateFormatter($GLOBALS['language'], IntlDateFormatter::NONE, IntlDateFormatter::MEDIUM))->format($this->event->start->timestamp())) ?>)
+<?php echo $this->subject ?>
+
+<?php echo $this->header ?>
 
 
+<?php echo _("When:") ?> <?php echo (new IntlDateFormatter($GLOBALS['language'], IntlDateFormatter::SHORT, IntlDateFormatter::MEDIUM))->format($this->event->start->timestamp()) ?> – <?php echo (new IntlDateFormatter($GLOBALS['language'], IntlDateFormatter::SHORT, IntlDateFormatter::MEDIUM))->format($this->event->end->timestamp()) ?>
+
+<?php if (strlen($this->recurrence)): ?>
+<?php echo _("Recurrence") ?>:
+
+<?php echo $this->recurrence ?>
+
+<?php endif ?>
 <?php if (strlen($this->event->location)): ?>
 <?php echo _("Location:") ?> <?php echo $this->event->location ?>
 
+<?php endif ?>
+<?php if (!empty($this->organizer)): ?>
+<?php echo _("Organizer:") ?> <?php echo $this->organizer ?>
 
 <?php endif ?>
 <?php if (count($this->event->attendees)): ?>
-<?php echo _("Attendees:") ?> <?php echo $this->event->attendees ?>
+<?php echo _("Attendees:") ?>
+<?php foreach ($this->event->attendees as $attendee): ?>
+- <?php echo is_null($attendee->addressObject->host) ? $attendee->displayName : $attendee->displayName . ' <' . $attendee->email . '>' ?>
 
-
+<?php endforeach ?>
 <?php endif ?>
 <?php if (strlen($this->event->description)): ?>
-<?php echo _("The following is a more detailed description of the event:") ?>
-
+<?php echo _("Description:") ?>
 
 <?php echo $this->event->description ?>
 
-
 <?php endif ?>
-<?php echo _("Attached is an iCalendar file with more information about the event. If your mail client supports iTip requests you can use this file to easily update your local copy of the event.") ?>
 <?php if ($this->linkAccept): ?>
+<?php echo _("Respond to this invitation:") ?>
 
+<?php echo _("Accept:") ?> <?php echo $this->linkAccept ?>
 
-<?php echo _("If your email client doesn't support iTip requests you can use one of the following links to accept or decline the event.") ?>
+<?php echo _("Tentative:") ?> <?php echo $this->linkTentative ?>
 
-
-<?php echo _("To accept the event:") ?>
-
-<?php echo $this->linkAccept ?>
-
-
-<?php echo _("To accept the event tentatively:") ?>
-
-<?php echo $this->linkTentative ?>
-
-
-<?php echo _("To decline the event:") ?>
-
-<?php echo $this->linkDecline ?>
+<?php echo _("Decline:") ?> <?php echo $this->linkDecline ?>
 
 <?php endif ?>
